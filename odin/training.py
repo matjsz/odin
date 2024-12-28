2ec472e7bcd44fca5303355ba49f00f22f08e899
Matheus Silva <54675543+matjsz@users.noreply.github.com>
2024-12-28 19:29:07 -0300

    feat: handler functions and ux improvements

--- odin/training.py
+++ odin/training.py
@@ -1,10 +1,14 @@
 
 import datetime
+import json
 import logging
 import os
 import shutil
 
 from colorama import Fore
+import ultralytics
+
+from results import YOLOResult
 
 
 class BaseTrainingCommands:
@@ -12,6 +16,28 @@ class BaseTrainingCommands:
         self.type = type
         self.dataset_name = dataset_name
         self.dataset_path = f"{os.path.abspath('.')}\\datasets\\{dataset_name}"
+        
+    def _get_chronicle_data(self, chronicle_path):
+        with open(chronicle_path+"\\chronicle.json", "r", encoding="utf8") as f:
+            data = json.loads(f.read())
+        return data
+        
+    def _gather_yolo_results(self, results) -> list[YOLOResult]:
+        odin_results = []
+        
+        for raw_result in results:
+            result = YOLOResult(
+                image_with_bboxes=raw_result.plot(),
+                inference_speed=raw_result.speed
+            )
+            odin_results.append(result)
+        
+        return odin_results
+        
+    def _check_if_eligible_model(self, model_file_name):
+        if model_file_name.startswith("epoch") or model_file_name in ["best.pt", "last.pt"]:
+            return False
+        return True
         
     def _try_create_folder(self, folder_path):
         try:
